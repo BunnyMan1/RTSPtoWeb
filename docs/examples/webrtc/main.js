df57064b8264961c6491cc5c1738ddb18ad76411
Sainath A <43345486+BunnyMan1@users.noreply.github.com>
2022-05-19 22:52:14 +0530

    Auth to WebRTC stream, example add btns

--- docs/examples/webrtc/main.js
+++ docs/examples/webrtc/main.js
@@ -1,52 +1,71 @@
-document.addEventListener('DOMContentLoaded', function () {
-  function startPlay (videoEl, url) {
-    const webrtc = new RTCPeerConnection({
-      iceServers: [{
-        urls: ['stun:stun.l.google.com:19302']
-      }],
-      sdpSemantics: 'unified-plan'
-    })
-    webrtc.ontrack = function (event) {
-      console.log(event.streams.length + ' track is delivered')
-      videoEl.srcObject = event.streams[0]
-      videoEl.play()
-    }
-    webrtc.addTransceiver('video', { direction: 'sendrecv' })
-    webrtc.onnegotiationneeded = async function handleNegotiationNeeded () {
-      const offer = await webrtc.createOffer()
+let webrtcObj = null;
 
-      await webrtc.setLocalDescription(offer)
+function startPlay(videoEl, url) {
+   const webrtc = new RTCPeerConnection({
+      iceServers: [
+         {
+            urls: ["stun:stun.l.google.com:19302"],
+         },
+      ],
+      sdpSemantics: "unified-plan",
+   });
+   webrtc.ontrack = function (event) {
+      console.log(event.streams.length + " track is delivered");
+      videoEl.srcObject = event.streams[0];
+      videoEl.play();
+   };
+   webrtc.addTransceiver("video", { direction: "sendrecv" });
+   webrtc.onnegotiationneeded = async function handleNegotiationNeeded() {
+      const offer = await webrtc.createOffer();
+
+      await webrtc.setLocalDescription(offer);
 
       fetch(url, {
-        method: 'POST',
-        body: new URLSearchParams({ data: btoa(webrtc.localDescription.sdp) })
+         method: "POST",
+         body: new URLSearchParams({
+            data: btoa(webrtc.localDescription.sdp),
+         }),
       })
-        .then(response => response.text())
-        .then(data => {
-          try {
-            webrtc.setRemoteDescription(
-              new RTCSessionDescription({ type: 'answer', sdp: atob(data) })
-            )
-          } catch (e) {
-            console.warn(e)
-          }
-        })
-    }
+         .then((response) => response.text())
+         .then((data) => {
+            try {
+               webrtc.setRemoteDescription(
+                  new RTCSessionDescription({
+                     type: "answer",
+                     sdp: atob(data),
+                  })
+               );
+            } catch (e) {
+               console.warn(e);
+            }
+         });
+   };
 
-    const webrtcSendChannel = webrtc.createDataChannel('rtsptowebSendChannel')
-    webrtcSendChannel.onopen = (event) => {
-      console.log(`${webrtcSendChannel.label} has opened`)
-      webrtcSendChannel.send('ping')
-    }
-    webrtcSendChannel.onclose = (_event) => {
-      console.log(`${webrtcSendChannel.label} has closed`)
-      startPlay(videoEl, url)
-    }
-    webrtcSendChannel.onmessage = event => console.log(event.data)
-  }
+   const webrtcSendChannel = webrtc.createDataChannel("rtsptowebSendChannel");
+   webrtcSendChannel.onopen = (event) => {
+      console.log(`${webrtcSendChannel.label} has opened`);
+      webrtcSendChannel.send("ping");
+   };
+   webrtcSendChannel.onclose = (_event) => {
+      console.log(`${webrtcSendChannel.label} has closed`);
+      // startPlay(videoEl, url);
+   };
+   webrtcSendChannel.onmessage = (event) => console.log(event.data);
+   webrtcObj = webrtc;
+}
 
-  const videoEl = document.querySelector('#webrtc-video')
-  const webrtcUrl = document.querySelector('#webrtc-url').value
+function stopPlay() {
+   webrtcObj?.close();
+   webrtcObj = null;
+}
 
-  startPlay(videoEl, webrtcUrl)
-})
+document.addEventListener("DOMContentLoaded", function () {});
+
+const videoEl = document.querySelector("#webrtc-video");
+const webrtcUrl = document.querySelector("#webrtc-url").value;
+
+const startBtn = document.querySelector("#btn-start");
+const endBtn = document.querySelector("#btn-end");
+
+startBtn.addEventListener("click", () => startPlay(videoEl, webrtcUrl));
+endBtn.addEventListener("click", stopPlay);
